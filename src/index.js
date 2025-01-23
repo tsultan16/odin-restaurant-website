3a31b8d9e39e7581da746402541cc39673732445
tsultan16 <tanzids@student.unimelb.edu.au>
2025-01-23 19:44:51 +1100

    added button event listeres for page tab switching

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {loadHomePage} from "./homePage.js";
+import {loadMenuPage} from "./menuPage.js";
 import "./style.css";
 
 console.log("Hello World!");
@@ -7,6 +8,20 @@ console.log("Hello World!");
 // render homepage content
 loadHomePage()
 
+// add event listeners to nav buttons
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const contact = document.querySelector("#contact");
+
+home.addEventListener("click", (event) => {
+    console.log("Loading home page.");
+    loadHomePage();
+});
+
+menu.addEventListener("click", (event) => {
+    console.log("Loading menu page.");
+    loadMenuPage();
+});
 
 
 
